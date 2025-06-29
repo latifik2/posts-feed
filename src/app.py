d00f6b9ad967d5ab5fb404c178f8f60408eb75fe
Королёв Латиф Александрович <Korolyov.LA@dns-shop.ru>
2025-06-29 16:11:59 +1000

    fix vuln, found by bandit

--- src/app.py
+++ src/app.py
@@ -380,4 +380,4 @@ def get_username(user_id):
     return "Неизвестный пользователь"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
